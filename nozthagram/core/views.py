240ac6c22f199954ddeae6cc6477b2c874699d37
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-19 02:28:19 +0100

    added some views:- likepost, post, upload

--- nozthagram/core/views.py
+++ nozthagram/core/views.py
@@ -1,11 +1,75 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+
 # Create your views here.
 
+
+@login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username = request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    posts = Post.objects.all()
+
+    context =  {'user_profile':user_profile,'posts':posts}
+    return render(request, 'index.html', context)
+
+
+@login_required(login_url='signin')
+def upload(request):
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return HttpResponse('<h1> upload pictures </h1>')
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    pass
+    
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+
+    return render(request, 'setting.html', {'user_profile':user_profile})
 
 
 def signup(request):
@@ -27,10 +91,13 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords do not match!!')
             return redirect('signup')
@@ -57,6 +124,7 @@ def signin(request):
         return render(request, 'signin.html')  
 
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
