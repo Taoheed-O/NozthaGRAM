6436b9b698bee65f45e9973f68a57d95955a98d3
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-19 02:21:26 +0100

    bug fix

--- nozthagram/core/models.py
+++ nozthagram/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 
 
@@ -19,3 +21,27 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created = models.DateTimeField(default=datetime.now)
+    likes = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.user
+    
+
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.username
