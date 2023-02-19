95129c1354d4f23ee9294e67c7f1a8a89a3966f5
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-19 02:43:08 +0100

    incrementing the no of likes on a post.

--- nozthagram/core/views.py
+++ nozthagram/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
@@ -37,8 +37,25 @@ def upload(request):
 
 @login_required(login_url='signin')
 def like_post(request):
-    pass
-    
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.likes = post.likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.likes = post.likes-1
+        post.save()
+        return redirect('/')
+
 
 
 @login_required(login_url='signin')
