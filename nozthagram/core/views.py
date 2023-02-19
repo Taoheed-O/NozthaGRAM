7d282afd6975c79fb6f5c64882c909481216af49
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-19 16:07:42 +0100

    suggestion feature added

--- nozthagram/core/views.py
+++ nozthagram/core/views.py
@@ -5,6 +5,7 @@ from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from itertools import chain
+import random 
 
 
 # Create your views here.
@@ -29,9 +30,36 @@ def index(request):
 
     feed_list = list(chain(*feed))
 
-    posts = Post.objects.all()
+    
+    all_users = User.objects.all()
+    user_following_all = []
 
-    context =  {'user_profile':user_profile,'posts':feed_list}
+    for user in user_following:
+        user_list = User.objects.get(username=user.user)
+        user_following_all.append(user_list)
+    
+    new_suggestions_list = [x for x in list(all_users) if x not in list(user_following_all())]
+    current_user = User.objects.filter(username = request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if x not in list(current_user)]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    
+    for ids in username_profile:
+        profile_lists = Profile.objects.filter(id_user=ids)
+        username_profile_list.append(profile_lists)
+    
+    suggestions_username_profile_list = list(chain(*username_profile_list))[:4]
+
+
+
+    context =  {'user_profile':user_profile, 
+                'posts':feed_list, 
+                'suggestions_username_profile_list':suggestions_username_profile_list}
     return render(request, 'index.html', context)
 
 
