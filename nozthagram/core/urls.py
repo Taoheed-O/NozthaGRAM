2a4c5e1810e74a532a00a368f717a2c7bc688cb8
Taoheed Oyeniyi <oyeniyiemperor@gmail.com>
2023-02-19 02:28:49 +0100

    code review

--- nozthagram/core/urls.py
+++ nozthagram/core/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
-    path('logout', views.logout, name='logout')
+    path('upload', views.upload, name='upload'),
+    path('like-post', views.like_post, name='like-post'),
+    path('logout', views.logout, name='logout'),
+    path('settings', views.settings, name='settings')
 
 ]
